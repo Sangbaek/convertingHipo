1d92a076fabe2a29c5c9181d6a29bc956418d44f
robertej19 <robertej@mit.edu>
2021-07-26 11:11:54 -0400

    pion variable included, pid printout

--- minimal/convertRec.cpp
+++ minimal/convertRec.cpp
@@ -205,6 +205,9 @@ int main(int argc, char **argv){
                 auto tGenVx = c12.getBank(idx_GenPart)->getFloat(iGenVx,ipa);
                 auto tGenVy = c12.getBank(idx_GenPart)->getFloat(iGenVy,ipa);
                 auto tGenVz = c12.getBank(idx_GenPart)->getFloat(iGenVz,ipa);
+
+		        cout<<" PID Value is: = "(c12.getBank(idx_GenPart)->getInt(iGenPid,ipa))<<<<endl;
+
  
             	if( (c12.getBank(idx_GenPart)->getInt(iGenPid,ipa)) == 11  ){  // electrons
     				GenEpx = tGenPx;
@@ -220,6 +223,15 @@ int main(int argc, char **argv){
             		GenPpy = tGenPy;
             		GenPpz = tGenPz;
             	}
+
+                if((c12.getBank(idx_GenPart)->getInt(iPid,ipa)) == 111  ){  // pions
+                    GenPipx = tPx;
+                    GenPipy = tPy;
+                    GenPipz = tPz;
+                    
+                }
+
+
                     
             	if((c12.getBank(idx_GenPart)->getInt(iGenPid,ipa)) == 22  ){  // photons
             		GenGpx[nmG] = tGenPx;
