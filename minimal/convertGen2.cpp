c9d8771344da0d0e8e9cee558615d1aa27aee8bf
Sangbaek <sangbaek@mit.edu>
2022-04-26 08:58:42 -0400

    universal cross Ref

--- minimal/convertGen2.cpp
+++ minimal/convertGen2.cpp
@@ -40,7 +40,10 @@ int main(int argc, char **argv){
     Int_t radMode;
     Float_t helicity;
 
-    Int_t crossRef;
+    Long_t crossRef;
+    Long_t nFile;
+    Long_t EventNumPre;
+    Long_t EventNumCur;
 
 // ===============    xB ==============    
     T->Branch("GenxB",&GenxB,"GenxB/F");
@@ -51,7 +54,7 @@ int main(int argc, char **argv){
     T->Branch("BornWeight",&BornWeight,"BornWeight/F");
     T->Branch("radMode",&radMode,"radMode/I");
     T->Branch("helicity",&helicity,"helicity/F");
-    T->Branch("crossRef",&crossRef,"crossRef/I");
+    T->Branch("crossRef",&crossRef,"crossRef/L");
   //
   //loop over files
   //
@@ -71,94 +74,8 @@ int main(int argc, char **argv){
       auto iWeight  = c12.getBankOrder(idx_GenEvent,"weight");
       auto iHelic = c12.getBankOrder(idx_GenEvent,"pbeam");
 
-        //  Filter bank created by Sangbaek
-        auto idx_FILTER = c12.addBank("FILTER::Index");
-        // auto iInd = c12.getBankOrder(idx_FILTER,"before");
-        auto iPcalSector = c12.getBankOrder(idx_FILTER, "pcal_sector");
-        auto iFtof1aSector = c12.getBankOrder(idx_FILTER, "ftof1a_sector");
-        auto iFtof1bSector = c12.getBankOrder(idx_FILTER, "ftof1b_sector");
-        auto iFtof2Sector = c12.getBankOrder(idx_FILTER, "ftof2_sector");
-        //=========
-
-        // Read banks: with DC, CVT, FTOF, LTCC, HTCC, ECAL, CTOF, CND 
-        auto idx_Traj = c12.addBank("REC::Traj");
-        auto iPindex = c12.getBankOrder(idx_Traj,"pindex");
-        auto iDetector = c12.getBankOrder(idx_Traj,"detector");
-        auto iLayer = c12.getBankOrder(idx_Traj,"layer");
-        auto iX = c12.getBankOrder(idx_Traj,"x");
-        auto iY = c12.getBankOrder(idx_Traj,"y");
-        auto iZ = c12.getBankOrder(idx_Traj,"z");
-        // ========================
-
-        // Scintillator bank
-        auto idx_RECScint = c12.addBank("REC::Scintillator");
-        auto jPindex = c12.getBankOrder(idx_RECScint,"pindex");
-        auto jDet = c12.getBankOrder(idx_RECScint,"detector");
-        auto jSec = c12.getBankOrder(idx_RECScint,"sector");
-        auto jLay = c12.getBankOrder(idx_RECScint,"layer");
-        auto jTim = c12.getBankOrder(idx_RECScint,"time");
-        auto jPat = c12.getBankOrder(idx_RECScint,"path");
-        auto jX   = c12.getBankOrder(idx_RECScint,"x");
-        auto jY = c12.getBankOrder(idx_RECScint,"y");
-        auto jZ = c12.getBankOrder(idx_RECScint,"z");
-
-        // REC::Track 
-        auto ldx_Track = c12.addBank("REC::Track");
-        auto lPindex = c12.getBankOrder(ldx_Track,"pindex");
-        auto lDetector = c12.getBankOrder(ldx_Track,"detector");
-        auto lsector = c12.getBankOrder(ldx_Track,"sector");
-        auto lq = c12.getBankOrder(ldx_Track,"q");
-        auto lchi2 = c12.getBankOrder(ldx_Track,"chi2");
-        auto lNDF = c12.getBankOrder(ldx_Track,"NDF");
-        // ========================
-
-        // Read banks: Calorimeter
-        auto mdx_Calo = c12.addBank("REC::Calorimeter");
-        auto mPindex = c12.getBankOrder(mdx_Calo,"pindex");
-        auto mDetector = c12.getBankOrder(mdx_Calo,"detector");
-        auto msector = c12.getBankOrder(mdx_Calo,"sector");
-        auto mLayer = c12.getBankOrder(mdx_Calo,"layer");
-        auto menergy = c12.getBankOrder(mdx_Calo,"energy");
-        auto mtime = c12.getBankOrder(mdx_Calo,"time");
-        auto mpath = c12.getBankOrder(mdx_Calo,"path");
-        auto mchi2 = c12.getBankOrder(mdx_Calo,"chi2");
-        auto mx = c12.getBankOrder(mdx_Calo,"x");
-        auto my = c12.getBankOrder(mdx_Calo,"y");
-        auto mz = c12.getBankOrder(mdx_Calo,"z");
-        auto mu = c12.getBankOrder(mdx_Calo,"lu");
-        auto mv = c12.getBankOrder(mdx_Calo,"lv");
-        auto mw = c12.getBankOrder(mdx_Calo,"lw");
-
-        // Read banks: Cherenkov (HTCC)
-        auto hdx_Cherenkov = c12.addBank("REC::Cherenkov");
-        auto hPindex = c12.getBankOrder(hdx_Cherenkov,"pindex");
-        auto hDetector = c12.getBankOrder(hdx_Cherenkov,"detector");
-        auto hnphe = c12.getBankOrder(hdx_Cherenkov,"nphe");
-
-        // Read banks: FT
-        auto ndx_FT = c12.addBank("REC::ForwardTagger");
-        auto nPindex = c12.getBankOrder(ndx_FT,"pindex");
-        auto nDetector = c12.getBankOrder(ndx_FT,"detector");
-        auto nLayer = c12.getBankOrder(ndx_FT,"layer");
-        auto nenergy = c12.getBankOrder(ndx_FT,"energy");
-        auto ntime = c12.getBankOrder(ndx_FT,"time");
-        auto npath = c12.getBankOrder(ndx_FT,"path");
-        auto nchi2 = c12.getBankOrder(ndx_FT,"chi2");
-        auto nradius = c12.getBankOrder(ndx_FT,"radius");
-        auto nx = c12.getBankOrder(ndx_FT,"x");
-        auto ny = c12.getBankOrder(ndx_FT,"y");
-        auto nz = c12.getBankOrder(ndx_FT,"z");
-
-        // MC bank
-        auto idx_GenPart = c12.addBank("MC::Particle");
-        auto iGenPid = c12.getBankOrder(idx_GenPart,"pid");
-        auto iGenPx  = c12.getBankOrder(idx_GenPart,"px");
-        auto iGenPy  = c12.getBankOrder(idx_GenPart,"py");
-        auto iGenPz  = c12.getBankOrder(idx_GenPart,"pz");
-        auto iGenVx  = c12.getBankOrder(idx_GenPart,"vx");
-        auto iGenVy  = c12.getBankOrder(idx_GenPart,"vy");
-        auto iGenVz  = c12.getBankOrder(idx_GenPart,"vz");
-
+      nFile = 0;
+      EventNumPre = 0;
       crossRef = 0;
 
         while(c12.next() == true){
@@ -199,6 +116,20 @@ int main(int argc, char **argv){
                   helicity = tHelic; //pol.
               }
           }
+
+          // Run config bank
+          for(auto ipa1 = 0; ipa1<c12.getBank(idx_RUNCon)->getRows();ipa1++){
+              auto tempR = c12.getBank(idx_RUNCon)->getInt(brun,ipa1);
+              auto tempE = c12.getBank(idx_RUNCon)->getInt(bevent,ipa1);
+              auto tempT = c12.getBank(idx_RUNCon)->getLong(btrigger,ipa1);
+              
+              EventNumCur = tempE;
+          }
+
+          if (EventNumCur<EventNumPre)  nFile++;
+          crossRef = nFile * 10000 + EventNumCur;
+          EventNumPre = EventNumCur;
+
 
           if (nmG>0) T->Fill();
 
