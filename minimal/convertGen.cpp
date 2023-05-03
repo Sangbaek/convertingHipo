47b78118d835e6240da7a8b8f5308f9d26fff2c4
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-03 16:23:23 -0500

    Save other informations

--- minimal/convertGen.cpp
+++ minimal/convertGen.cpp
@@ -29,15 +29,39 @@ int main(int argc, char **argv){
     TFile *rFile = TFile::Open("genOnly.root","RECREATE");
     TTree *T=new TTree("T","Gen");
 
+   // === xB, Q2, t, phi====
+    T3Vector* beam = new T3Vector(0, 0, 10.604);
+    T3Vector* ele = new T3Vector();
+    T3Vector* pro = new T3Vector();
+    T3Vector* v3l = new T3Vector();
+    T3Vector* v3h = new T3Vector();
+    T3Vector* VGS = new T3Vector();
+    Float_t nu;
+
+    Float_t me = 0.5109989461 * 0.001;
+    Float_t M = 0.938272081;
+
+    Float_t GenxB;
+    Float_t GenQ2;
+    Float_t Gent;
+    Float_t Genphi;
+
    // =====  proton =====
     Float_t GenPpx;
     Float_t GenPpy;
     Float_t GenPpz;
+    Float_t GenPp;
+    Float_t GenPtheta;
+    Float_t GenPphi;
 
     // ==== electron =====
     Float_t GenEpx;
     Float_t GenEpy;
     Float_t GenEpz;
+
+    Float_t GenEp;
+    Float_t GenEtheta;
+    Float_t GenEphi;
     
     // ==== gammas =====
     Int_t nmG;
@@ -45,29 +69,53 @@ int main(int argc, char **argv){
     Float_t GenGpy[2];
     Float_t GenGpz[2];
 
+    Float_t GenGp[2];
+    Float_t GenGptheta[2];
+    Float_t GenGphi[2];
+
     // =====  pion =====
     Int_t nmpi;
 
     Float_t GenPipx;
     Float_t GenPipy;
     Float_t GenPipz;
+
+    Float_t GenPip;
+    Float_t GenPitheta;
+    Float_t GenPiphi;
     
+
+    T->Branch("GenxB",&GenxB,"GenxB/F");
+    T->Branch("GenQ2",&GenQ2,"GenQ2/F");
+    T->Branch("Gent",&Gent,"Gent/F");
+    T->Branch("Genphi",&Genphi,"Genphi/F");
 
 // ===============    Electrons ==============    
     T->Branch("GenEpx",&GenEpx,"GenEpx/F");
     T->Branch("GenEpy",&GenEpy,"GenEpy/F");
     T->Branch("GenEpz",&GenEpz,"GenEpz/F");
 
+    T->Branch("GenEp",&GenEp,"GenEp/F");
+    T->Branch("GenEtheta",&GenEtheta,"GenEtheta/F");
+    T->Branch("GenEphi",&GenEphi,"GenEphi/F");
+
 // ===============    Protons ================================== 
     T->Branch("GenPpx",&GenPpx,"GenPpx/F");
     T->Branch("GenPpy",&GenPpy,"GenPpy/F");
     T->Branch("GenPpz",&GenPpz,"GenPpz/F");
+
+    T->Branch("GenPp",&GenPp,"GenPp/F");
+    T->Branch("GenPtheta",&GenPtheta,"GenPtheta/F");
+    T->Branch("GenPphi",&GenPphi,"GenPphi/F");
 
 // ===============    Pions ================================== 
     T->Branch("GenPipx",&GenPipx,"GenPipx/F");
     T->Branch("GenPipy",&GenPipy,"GenPipy/F");
     T->Branch("GenPipz",&GenPipz,"GenPipz/F");
 
+    T->Branch("GenPip",&GenPip,"GenPip/F");
+    T->Branch("GenPitheta",&GenPitheta,"GenPitheta/F");
+    T->Branch("GenPiphi",&GenPiphi,"GenPiphi/F");
 
 
 // ================   Gammas  ===============    
@@ -76,6 +124,9 @@ int main(int argc, char **argv){
     T->Branch("GenGpy",&GenGpy,"GenGpy[nmG]/F");
     T->Branch("GenGpz",&GenGpz,"GenGpz[nmG]/F");
 
+    T->Branch("GenGp",&GenGp,"GenGp/F");
+    T->Branch("GenGtheta",&GenGtheta,"GenGtheta/F");
+    T->Branch("GenGphi",&GenGphi,"GenGphi/F");
 
   //
   //loop over files
@@ -106,6 +157,10 @@ int main(int argc, char **argv){
                   GenEpx = tPx;
                   GenEpy = tPy;
                   GenEpz = tPz;
+
+                  GenEp     = TMath::Sqrt(tPx*tPx + tPy*tPy + tPz*tPz);
+                  GenEtheta = (180.0/TMath::Pi())*TMath::ATan2(TMath::Sqrt(tPx*tPx+tPy*tPy),tPz);
+                  GenEphi   = (180.0/TMath::Pi())*TMath::ATan2(tPy,tPx);
               }
             
               if((c12.getBank(idx_GenPart)->getInt(iPid,ipa)) == 2212  ){  // protons
@@ -113,6 +168,9 @@ int main(int argc, char **argv){
                   GenPpy = tPy;
                   GenPpz = tPz;
 
+                  GenPp     = TMath::Sqrt(tPx*tPx + tPy*tPy + tPz*tPz);
+                  GenPtheta = (180.0/TMath::Pi())*TMath::ATan2(TMath::Sqrt(tPx*tPx+tPy*tPy),tPz);
+                  GenPphi   = (180.0/TMath::Pi())*TMath::ATan2(tPy,tPx);
             	}
 
               if((c12.getBank(idx_GenPart)->getInt(iPid,ipa)) == 111  ){  // pions
@@ -120,18 +178,42 @@ int main(int argc, char **argv){
                   GenPipy = tPy;
                   GenPipz = tPz;
                 
+                  GenPip     = TMath::Sqrt(tPx*tPx + tPy*tPy + tPz*tPz);
+                  GenPitheta = (180.0/TMath::Pi())*TMath::ATan2(TMath::Sqrt(tPx*tPx+tPy*tPy),tPz);
+                  GenPiphi   = (180.0/TMath::Pi())*TMath::ATan2(tPy,tPx);
               } // if for protons
                 
               if((c12.getBank(idx_GenPart)->getInt(iPid,ipa)) == 22  ){  // photons
                   GenGpx[nmG] = tPx;
                   GenGpy[nmG] = tPy;
                   GenGpz[nmG] = tPz;
+
+                  GenGp[nmG]     = TMath::Sqrt(tPx*tPx + tPy*tPy + tPz*tPz);
+                  GenGtheta[nmG] = (180.0/TMath::Pi())*TMath::ATan2(TMath::Sqrt(tPx*tPx+tPy*tPy),tPz);
+                  GenGphi[nmG]   = (180.0/TMath::Pi())*TMath::ATan2(tPy,tPx);
+
                   nmG++;
               }
           }
 
-          if (nmG>0) T->Fill();
+          ele.setXYZ(GenEpx, GenEpy, GenEpz);
+          pro.setXYZ(GenPpx, GenPpy, GenPpz);
+          VGS.SetXYZ(-GenEpx, -GenEpy, 10.604 - GenEpz);
 
+          nu     = 10.604 - TMath::Sqrt(GenEp*GenEp + me*me);
+          GenQ2  = -(nu*nu - VGS.Mag2());
+          GenxB  = Q2 / 2.0 / M / nu;
+          Gent   = 2 * M * (TMath::Sqrt(GenPp*GenPp+M*M) - M);
+
+          v3l = beam.Cross(ele);
+          v3h = pro.Cross(VGS);
+          Genphi = v3l.Angle(v3h);
+          if (v3l.Dot(pro) > 0) {
+            GenPhi = 2*TMath::Pi() - GenPhi;
+          }
+          GenPhi = (180.0/TMath::Pi())*GenPhi;
+
+          if (nmG>0) T->Fill();
         }
 
     }
